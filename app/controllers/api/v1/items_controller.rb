4d88ab26883ab6e0a5ba49fba826574f82ec69cc
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-03-28 15:57:10 -0600

    Add: create and destroy basic functionality

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,13 +1,43 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: ItemSerializer.new(Item.all)
+    render json: ItemSerializer.new(all_items)
+  end
+
+  def show
+    render json: ItemSerializer.new(item(items_params[:id]))
+  end
+
+  def create
+    @new_item = Item.create(items_params)
+
+    if @new_item.save
+      render json: ItemSerializer.new(@new_item), status: :created
+    else
+      # render json: ErrorSerializer.new(@new_item.errors), status:
+    end
+  end
+
+  def destroy
+    Item.destroy(item(items_params[:id]).id)
   end
 
   private
+  def item(id)
+    all_items.find(id)
+  end
+
+  def all_items
+    @_all_items ||= Item.all
+  end
 
   def items_params
     params.permit(
-      
+      :id,
+      :name,
+      :description,
+      :unit_price,
+      :merchant_id,
+      :item
     )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: :index
-      resources :merchants, only: [:index, :show] do
-        resources :items, only: :index
-      end
+      resources :items, except: [:new, :edit, :update]
+      resources :merchants, only: [:index, :show]
     end
   end
 end
