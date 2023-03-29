fcabc9f307ac3e3a43172d142b61564add7c883d
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-03-28 20:14:29 -0600

    Change: test for get index request

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,7 +1,6 @@
 class MerchantSerializer
   include JSONAPI::Serializer
   attributes :name, :items
-  has_many :items
   # def serializable_hash
   #   if super[:data].is_a?(Array)
   #     super
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -6,15 +6,34 @@ describe "Merchants API" do
 
     get '/api/v1/merchants'
 
-    expect(response).to be_successful
-    
-    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(response).to have_http_status(200)
 
-    expect(merchants.count).to eq(3)
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
-    merchants.each do |merchant|
-      merchant_name = Merchant.find(merchant[:id]).name
-      expect(merchant_name).to eq(merchant[:attributes][:name])
+    expect(merchants).to have_key(:data)
+
+    expect(merchants[:data].count).to eq(3)
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id].to_i).to be_an(Integer)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a(String)
+
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a(Hash)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+
+      expect(merchant[:attributes]).to have_key(:items)
+      expect(merchant[:attributes][:items]).to be_an(Array)
     end
+
+    # merchants.each do |merchant|
+    #   merchant_name = Merchant.find(merchant[:id]).name
+    #   expect(merchant_name).to eq(merchant[:attributes][:name])
+    # end
   end
 end
